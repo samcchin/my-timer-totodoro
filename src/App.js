f9ba108e17a126296bce8e286f02a8951f583280
Samantha Chin <samanthachin.dsp@gmail.com>
2023-06-19 20:24:18 -0700

    deployed website

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <div className="spotify_player">
       <iframe
         src="https://open.spotify.com/embed/playlist/7E1JPgviV96g5jQ649214c?utm_source=generator" width="25%"
+        title="embedded spotify player"
         frameBorder="0"
         allowfullscreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
--- src/components/timer/Clock/Clock.jsx
+++ src/components/timer/Clock/Clock.jsx
@@ -13,7 +13,7 @@ const Clock = () => {
             }, 1000);
             return () => clearInterval(interval);
         }
-    }, [time, isActive]);
+    }, [time, isActive, setTime]);
 
     const toggleClock = () => {
         setIsActive(!isActive);
@@ -36,11 +36,11 @@ const Clock = () => {
     <div className="clock_container">
         <div className="timer_text">{getTime(time)}</div>
         <button onClick={toggleClock} className="start_pause_button">
-            <img className="white-totoro" src={whiteTotoro} alt="Image"/>
+            <img className="white-totoro" src={whiteTotoro} alt="white totoro"/>
             {isActive ? "Pause":"Start"}
         </button>
         {time === 0 && <button onClick={resetTime} className="reset_button">
-            <img className="blue-totoro" src={blueTotoro} alt="Image"/>
+            <img className="blue-totoro" src={blueTotoro} alt="blue totoro"/>
             Reset
         </button>}
     </div>
